8dfb902c8803ec7b9d58a8b4871a510dabe4b5e2
Yatin Singla <yatinsingla2@gmail.com>
2020-05-07 05:03:12 -0700

    Design Music Controller

--- Term Project/ExerciseViewController.swift
+++ Term Project/ExerciseViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ExerciseViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    
+    @IBOutlet weak var CoverImage: UIImageView!
+    @IBOutlet weak var TrackTitle: UILabel!
+    @IBOutlet weak var ArtistTitle: UILabel!
+    @IBOutlet weak var TrackSlider: UISlider!
+    @IBOutlet weak var PlayPause: UIButton!
+    
+    @IBAction func PlayPauseTapped(_ sender: UIButton) {
+    }
+    @IBAction func NextTapped(_ sender: UIButton) {
+    }
+    @IBAction func PreviousTapped(_ sender: UIButton) {
+    }
+    @IBOutlet weak var LoginBtn: UIButton!
+    @IBAction func LoginBtnTapped(_ sender: UIButton) {
+    }
+    
     @IBAction func HandleSwipe(_ sender: UISwipeGestureRecognizer)
     {
         print("right Swipe Gesture Recognized")
@@ -18,10 +35,22 @@ class ExerciseViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        /*
+         if loggedin{
+            LoginBtn.isHidden = True
+         }
+         else{
+            everything else is hidden
+         }
+         */
+        LoginBtn.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: 8.0, bottom: 0.0, right: 0.0)
+        LoginBtn.imageEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 8.0)
+        LoginBtn.adjustsImageWhenHighlighted = false
+        LoginBtn.setImage(UIImage(named: "spotifylogo-32.png"), for: .normal)
+        
     }
-    
+
 
     /*
     // MARK: - Navigation
